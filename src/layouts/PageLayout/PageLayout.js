c11fee95a9840fbb7f1469636f4bd6d3279fa5ed
InfinityCoders <infiniticoder@gmail.com>
2018-02-12 17:38:25 +0530

    added pagelayout navigation

--- src/layouts/PageLayout/PageLayout.js
+++ src/layouts/PageLayout/PageLayout.js
@@ -4,13 +4,19 @@ import PropTypes from 'prop-types'
 import './PageLayout.scss'
 
 export const PageLayout = ({ children }) => (
-  <div className='container text-center'>
-    <h1>React Redux Starter Kit</h1>
-    <IndexLink to='/' activeClassName='page-layout__nav-item--active'>Home</IndexLink>
-    {' · '}
-    <Link to='/counter' activeClassName='page-layout__nav-item--active'>Counter</Link>
-    <div className='page-layout__viewport'>
-      {children}
+  <div className='container-fluid text-center'>
+    <div className='row bg-blue'>
+      <span className='logo'>TDM</span>
+      <div className='nav'>
+        <IndexLink to='/' activeClassName='page-layout__nav-item--active'>Home</IndexLink>
+        <Link to='/counter' activeClassName='page-layout__nav-item--active'>Counter</Link>
+      </div>
+      <span className='settings'></span>
+    </div>
+    <div className='row'>
+      <div className='page-layout__viewport'>
+        {children}
+      </div>    
     </div>
   </div>
 )
