44a0124bc1fd574ee05f7aa667cabf7b86258669
InfinityCoders <infiniticoder@gmail.com>
2018-02-23 19:30:06 +0530

    added socket and routes

--- server/index.js
+++ server/index.js
@@ -1,10 +1,32 @@
-var http = require('http');
+const http = require('http');
+var server = require('http').createServer();
+var io = require('socket.io')(server);
 
 require('./config/connectMongo');
 var route = require('./routes');
 var Auth = require('./model/auth');
 
-http.createServer(function(req, res) {
+var io = require('socket.io')(http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     route.routes(req, res);
-}).listen(5000);
+}).listen(5000));
+
+
+console.log('=======================');
+io.on('connection', function(client){
+    console.log('its connected');
+    res.write('Hello u are connected to sockets.');
+    client.on('event', function(){
+        console.log('event');
+        res.end();
+    });
+    client.on('disconnect', function(){
+        console.log('disconnected');
+    });
+});
+
+// server(function(req, res){
+//         res.writeHead(200, {'Content-Type': 'text/html'});
+//         route.routes(req, res);
+// });
+// server.listen(5000);
--- server/routes.js
+++ server/routes.js
@@ -1,45 +1,27 @@
 var url = require('url');
 var _ = require('lodash');
 
-exports.routes = function(req, res) {
-    console.log('URL', req.url, req.url.toString().length, '========= Routes')
-
+exports.routes = function(req, res, io) {
     let urlString = req.url.toString();
     let routeLevel = urlString.split('/').length;
-    const CASE = routeLevel == 2 ? 'index' : urlString.split('/')[1];
+    const CASE = routeLevel == 2 && urlString.indexOf('?') > -1 ? 'index' : urlString.split('/')[1];
+
+    console.log('URL', req.url, req.url.toString().length, CASE , '========= Routes')
+
     switch(CASE) {
         case 'index': {
             var q = url.parse(req.url, true).query;
-            const kitty = new Auth({
-                name: 'Abhishek',
-                username: {
-                    type: 'abhi',
-                    required: true,
-                    unique: true
-                },
-                password: {
-                    type: '123456',
-                    required: true
-                },
-                admin: true,
-                location: 'Delhi',
-                meta: {
-                  age: 25,
-                  website: '-'
-                },
-                created_at: Date,
-                updated_at: Date
-            });
             res.write('<h1>Hello World!</h1>');
             var txt = _.size(q) > 0 ? res.write(q.year + " " + q.month) : null;
             res.end();
             break;
         }
-        case 'form': {
-
+        case 'chat': {
+            res.end();
+            break;
         }
         default: {
-            res.write('<center><h1>OOPS! Error 404.</h1><h3> You might have landed on wrong url, It doesn\'t exist.</h3></center>');
+            res.write('<center><h1>OOPS! Error 404.</h1><h2>Humse Na hopaega.</h2><h3> You might have landed on wrong url, It doesn\'t exist.</h3></center>');
         }
     }
 }
